8639c25865dcb8e9854e30304f4d1b176005c51c
Bradley Bossard <bradleybossard@gmail.com>
2016-10-11 14:31:14 +0000

    Adding another unit test

--- src/index.spec.js
+++ src/index.spec.js
@@ -6,4 +6,9 @@ describe('iterate test', function() {
     const expandedString = fauna.iterate('a', {'a': 'bab'}, 2);
     expect(expandedString).to.be.equal('bbabb');
   });
+
+  it('simple iteration', function() {
+    const expandedString = fauna.iterate('a', {'a': 'bac', 'c': 'ddd'}, 4);
+    expect(expandedString).to.be.equal('bbbbacddddddddd');
+  });
 });

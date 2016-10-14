7da556b9721942932bcb56fc6ca85c89d02cbfae
Bradley Bossard <bradleybossard@gmail.com>
2016-10-14 17:04:23 +0000

    test(tests): Updated test case with more realistic input

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,8 +3,8 @@ var fauna = require('./index.js');
 
 describe('iterate test', function() {
   it('simple iteration', function() {
-    const expandedString = fauna.iterate('a', {'a': 'bab'}, 2);
-    expect(expandedString).to.be.equal('bbabb');
+    const expandedString = fauna.iterate('L', {'L': 'LFLR+'}, 2);
+    expect(expandedString).to.be.equal('LFLR+FLFLR+R+');
   });
 
   it('simple iteration', function() {
